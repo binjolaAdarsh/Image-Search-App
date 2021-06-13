d349da53579ac8dc0f9f7bf1b787f522fda1f763
adarsh <adarshbinjola2@gmail.com>
2021-06-13 17:51:24 +0530

    handle process death in view model

--- app/src/main/java/adarsh/learn/imagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/adarsh/learn/imagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -1,16 +1,16 @@
 package adarsh.learn.imagesearchapp.ui.gallery
 
 import adarsh.learn.imagesearchapp.data.Repo
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 
-class GalleryViewModel @ViewModelInject constructor(private val repo: Repo) : ViewModel() {
+class GalleryViewModel @ViewModelInject constructor(private val repo: Repo,
+                                                    @Assisted state:SavedStateHandle // for handling the process death
+) : ViewModel() {
 
-    private val imageQuery = MutableLiveData(DEFAULT_IMAGE_QUERY)
+    private val imageQuery =state.getLiveData(CURRENT_QUERY, DEFAULT_IMAGE_QUERY)
 
     // when ever the value of imageQuery livedata changes this switchMap block gets executes
     val photos = imageQuery.switchMap { searchString->
@@ -22,6 +22,7 @@ class GalleryViewModel @ViewModelInject constructor(private val repo: Repo) : Vi
     }
 
     companion object{
+        private const val CURRENT_QUERY ="current_query"
         private const val DEFAULT_IMAGE_QUERY ="dog"
     }
 }
